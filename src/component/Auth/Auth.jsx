a8d9fd00f5598110cf8e2ec17955588ca472e7ea
rahulfordev <rahulforsms@gmail.com>
2023-12-13 17:50:35 +0600

    signIn with google added

--- src/component/Auth/Auth.jsx
+++ src/component/Auth/Auth.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import { app } from "../../config/firebase";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
@@ -8,6 +15,7 @@ const Auth = () => {
 
   // console.log(auth?.currentUser?.providerData);
   const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
 
   const signUP = async () => {
     try {
@@ -18,39 +26,51 @@ const Auth = () => {
     }
   };
 
-  // const signIn = async () => {
-  //   try {
-  //     await signInWithEmailAndPassword(auth, email, password);
-  //     console.log("Sign In successful");
-  //   } catch (error) {
-  //     console.log(error.message);
-  //   }
-  // };
+  const signIn = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("Sign In successful");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-  // const SignOut = async () => {
-  //   try {
-  //     await signOut(auth);
-  //     console.log("Sign Out  Successful");
-  //   } catch (error) {
-  //     console.log(error.message);
-  //   }
-  // };
+  const SignOut = async () => {
+    try {
+      await signOut(auth);
+      console.log("Sign Out  Successful");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const SignInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, provider);
+      console.log("Goole signIn SuccessFully completed");
+    } catch (error) {
+      console.log("Google Signin " + error.message);
+    }
+  };
 
   return (
     <div>
-      <input
-        onChange={(e) => setEmail(e.target.value)}
-        type="email"
-        placeholder="Email"
-      />
-      <input
-        onChange={(e) => setPassword(e.target.value)}
-        type="password"
-        placeholder="Password"
-      />
-      <button onClick={signUP}>Sign Up</button>
-      {/* <button onClick={signIn}>Sign In</button>
-      <button onClick={SignOut}>Sign Out</button> */}
+      <div>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+        />
+        <button onClick={signUP}>Sign Up</button>
+        <button onClick={signIn}>Sign In</button>
+        <button onClick={SignOut}>Sign Out</button>
+        <button onClick={SignInWithGoogle}>Sign In With Google</button>
+      </div>
     </div>
   );
 };
